f2df3d31bc3235be402b619ef0fda0743221af57
caseyli01 <93268539+caseyli01@users.noreply.github.com>
2024-05-27 00:34:38 +0200

    update

--- mof_build_demo/MDprepare/move2C.py
+++ mof_build_demo/MDprepare/move2C.py
@@ -14,11 +14,11 @@ def atoms2c(output,dx,dy,dz,x,y,z):
             value_resnumber = int((lines[i])[0:5])
             value_resname = lines[i][5:10]
             
-            value_label = values[1] 
-            value_atom_number = int(values[2]) 
-            value_x = float(values[3])+dx #x      
-            value_y = float(values[4])+dy#y
-            value_z = float(values[5])+dz #z
+            value_label = lines[i][10:15]
+            value_atom_number = int(lines[i][15:20]) 
+            value_x = float(lines[i][20:28])+dx #x      
+            value_y = float(lines[i][28:36])+dy#y
+            value_z = float(lines[i][36:44])+dz #z
 
             formatted_line = "%5d%-5s%5s%5d%8.3f%8.3f%8.3f" % (
                         value_resnumber, value_resname, value_label, value_atom_number, value_x, value_y, value_z
